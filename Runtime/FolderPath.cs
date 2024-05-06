9a9b90271a0b2aa426f1e9b549748fb8deff1061
Kévin Reilhac <41150339+KevinReilhac@users.noreply.github.com>
2024-05-06 17:11:37 +0200

    Remove implicit cast

--- Runtime/FolderPath.cs
+++ Runtime/FolderPath.cs
@@ -13,11 +13,11 @@ namespace UltimateFolderPath
     {
         [SerializeField] public string _path = null;
 
-        public FolderPath(string path) {this._path = path;}
+        public FolderPath(string path) { this._path = path; }
 
         public string AbsolutePath => PathUtils.Join(RelativeTo, _path).ClearPath();
         public string RelativePath => _path.ClearPath();
-        public virtual string RelativeTo {get => null;}
+        public virtual string RelativeTo { get => null; }
 
         public string[] GetAllFiles(bool relative = true)
         {
@@ -25,6 +25,5 @@ namespace UltimateFolderPath
             return files.Select(f => Path.GetRelativePath(RelativeTo, f)).ToArray();
         }
 
-        public static implicit operator string(FolderPath f) => f.RelativePath;
     }
 }
